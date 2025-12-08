d893cc8865e259d82f66e86216827ad472df6d6e
ctyoung22 <connertyoung22@gmail.com>
2025-12-08 00:06:45 -0600

    Refactor map and character rendering

--- src/PacModel.java
+++ src/PacModel.java
@@ -1,6 +1,45 @@
 import java.util.ArrayList;
 
 public class PacModel {
-    public ArrayList<Pickup> pickups;
+    private final int tileSize = 32;
+    private final int rowCount = 21;
+    private final int columnCount = 19;
+    private final int boardWidth = columnCount * tileSize;
+    private final int boardHeight = rowCount * tileSize;
+
+    private PacMan pacman;
+    private GhostManager ghostManager;
     
+    public PacModel() {
+        pacman = new PacMan(9 * tileSize, 15 * tileSize, tileSize, boardWidth, boardHeight); // create Pac
+        ghostManager = new GhostManager(tileSize);
+    }
+
+    public PacMan getPacman() {
+        return pacman;
+    }
+
+    public GhostManager getGhostManager() {
+        return ghostManager;
+    }
+
+    public int getTileSize() {
+        return tileSize;
+    }
+
+    public int getBoardWidth() {
+        return boardWidth;
+    }
+
+    public int getBoardHeight() {
+        return boardHeight;
+    }
+
+    public int getRowCount() {
+        return rowCount;
+    }
+
+    public int getColumnCount() {
+        return columnCount;
+    }
 }
